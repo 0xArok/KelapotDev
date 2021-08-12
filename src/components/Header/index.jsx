6467c7da43451730fd8d44a3e61ee2206661ce9c
SPKJ <davanoraindikaprivate@gmail.com>
2021-08-12 17:32:57 +0800

    header scroll

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -31,7 +31,7 @@ export default function Header() {
       <div className="header-container">
       
       <h1 className="header-h1" id="header-text" data-scroll data-scroll-direction="horizontal" data-scroll-speed="-6" data-scroll-class="appear" data-scroll-repeat="true">Hello, We Are Kelapot</h1>
-      <img src="https://i.ibb.co/dPZN29f/DSCF8355-min.jpg" data-scroll data-scroll-direction="vertical" data-scroll-speed="-3" data-scroll-class="appear" data-scroll-repeat="true"/>
+      <img src="https://i.ibb.co/dPZN29f/DSCF8355-min.jpg" data-scroll data-scroll-direction="vertical" data-scroll-speed="-6" data-scroll-class="appear" data-scroll-repeat="true"/>
       
       </div>
     </section>
