52ece03ab60ba0b9222f14b38d51141b421fc434
SPKJ <davanoraindikaprivate@gmail.com>
2021-08-12 11:38:10 +0800

    headerimages

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -28,9 +28,9 @@ export default function Header() {
 
   return (
     <section className="header-container" data-scroll-section>
-      <div className="header-container">
+      
       <img className="header-img" src="https://i.ibb.co/dPZN29f/DSCF8355-min.jpg" />
-      </div>
+      
       
       
       <h1 id="header-text">Hello, We Are Kelapot</h1>
