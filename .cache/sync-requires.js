22811d95629ee713d8db15ac261077ae6759317e
cult.arc <davanoraindikaprivate@gmail.com>
2021-09-21 23:39:51 +0800

    test

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\studiomerah\\Desktop\\kelapotv3\\KelapotDev\\.cache\\dev-404-page.js"))),
   "component---src-pages-index-jsx": hot(preferDefault(require("C:\\Users\\studiomerah\\Desktop\\kelapotv3\\KelapotDev\\src\\pages\\index.jsx")))
 }
 
